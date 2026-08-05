486641abc4235673d7d3d67ea25ac35e7cf48596
Kamal Arumugam <kamalak1506@gmail.com>
2026-08-05 08:07:56 +0530

    Encapsulation implementation

--- encapsulation.java
+++ encapsulation.java
@@ -3,30 +3,35 @@ private, and exposing controlled access to that data only through
 public methods (commonly called getters and setters), which can
 enforce validation rules, logging, or other logic at the point of access.*/
 
+class BankAccount{
+	private String name;
+	private double balance;
 
-class BankAccount {
- private double balance; // hidden — no outside code 
- public double getBalance() { // getter — con 
-   return balance;
- }
- public void deposit(double amount) { // controlled W 
-   if (amount <= 0) {
-   System.out.println("Deposit must be positive! ")
-   return;
-   }
-   balance += amount;
- }
- public void withdraw(double amount) {
-   if (amount > balance) {
-     System.out.println("Insufficient funds!");
-     return;
-   }
-   balance -= amount;
- }
+	BankAccount(String name, double initialAmount){
+		this.name = name;
+		if(initialAmount > 0){
+			this.balance = initialAmount;
+		}
+	}
+	public String getName(){
+		return name;
+	}
+	public int getBalance(){
+		return (int)balance;
+	}
+	public void deposit(double amount){
+		if(amount > 0){
+			balance += amount;
+		}
+	}
 }
 
-BankAccount acc = new BankAccount();
-acc.deposit(1000);
-acc.withdraw(200);
-System.out.println(acc.getBalance()); // 800
-// acc.balance = -5000; 
+class Main{
+	public static void main(String args[]){
+		BankAccount acc = new BankAccount("Kamal", 50000.80);
+		System.out.println(acc.getName());
+		acc.deposit(30000);
+		System.out.println(acc.getBalance());
+		// System.out.println(acc.balance);
+	}
+}
